40b5e23bcef9b5c3f30ec78a4657842acd57a191
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-06 19:40:48 +0300

    update

--- build.sh
+++ build.sh
@@ -7,22 +7,22 @@
 set -euo pipefail
 
 HERE=$(dirname "$BASH_SOURCE")
-
-i686-elf-as "$HERE/boot.s" -o "$HERE/boot.o" # paru i686-elf-gcc
-
-i686-elf-gcc -c "$HERE/kernel.c" -o "$HERE/kernel.o" -std=gnu99 -ffreestanding -O2 -Wall -Wextra
-
-i686-elf-gcc -T "$HERE/linker.ld" -o "$HERE/myos.bin" -ffreestanding -O2 -nostdlib boot.o kernel.o -lgcc
-
-grub-file --is-x86-multiboot "$HERE/myos.bin" # sudo pacman -S grub
-
 FOLDER_RELEASE="$HERE/release"
 FOLDER_ISO="$FOLDER_RELEASE/iso"
+FOLDER_BOOT="$HERE/boot"
+
+i686-elf-as "$FOLDER_BOOT/boot.s" -o "$FOLDER_RELEASE/boot.o" # paru i686-elf-gcc
+
+i686-elf-gcc -c "$HERE/kernel.c" -o "$FOLDER_RELEASE/kernel.o" -std=gnu99 -ffreestanding -O2 -Wall -Wextra
+
+i686-elf-gcc -T "$FOLDER_BOOT/linker.ld" -o "$FOLDER_RELEASE/myos.bin" -ffreestanding -O2 -nostdlib "$FOLDER_RELEASE/boot.o" "$FOLDER_RELEASE/kernel.o" -lgcc
+
+grub-file --is-x86-multiboot "$FOLDER_RELEASE/myos.bin" # sudo pacman -S grub
 
 rm -rf "$FOLDER_ISO"
 mkdir -p "$FOLDER_ISO/boot/grub"
-cp "$HERE/myos.bin" "$FOLDER_ISO/boot/myos.bin"
-cp "$HERE/grub.cfg" "$FOLDER_ISO/boot/grub/grub.cfg"
+mv "$FOLDER_RELEASE/myos.bin" "$FOLDER_ISO/boot/myos.bin"
+cp "$FOLDER_BOOT/grub.cfg" "$FOLDER_ISO/boot/grub/grub.cfg"
 grub-mkrescue -o "$FOLDER_RELEASE/myos.iso" "$FOLDER_ISO" # sudo pacman -S libisoburn
 
-qemu-system-i386 -cdrom myos.iso
+qemu-system-i386 -cdrom "$FOLDER_RELEASE/myos.iso"
