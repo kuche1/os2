9d28b5f39d060433ac585f8af250aefbeb1a2c49
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-07 00:52:58 +0300

    update

--- src/shell.c
+++ src/shell.c
@@ -1,25 +1,28 @@
 
+#define shell$CMD_MAXLEN 50
+
 void shell$main(void){
 
     out$cstr("Welcome to ligma shell\n");
     out$cstr("\n");
 
 	// TODO reutrn code
+	// TODO history of commands
 
 	while(true){
 
 		out$cstr("> ");
 
-		arr$INIT(char, line, 9999);
-		in$line(line);
+		arr$INIT(char, cmd, shell$CMD_MAXLEN);
+		in$line(cmd);
 
 		out$cstr("you just entered: ");
-		out$str(line);
+		out$str(cmd);
 		out$nl();
 
-		if(arr$char$same_as$cstr(line, "nc")){
+		if(arr$char$same_as$cstr(cmd, "nc")){
 			out$terminal_next_visible_color();
-		}else if(arr$char$same_as$cstr(line, "pc")){
+		}else if(arr$char$same_as$cstr(cmd, "pc")){
 			out$terminal_previous_visible_color();
 		}else{
 			out$cstr("unknown command\n");
