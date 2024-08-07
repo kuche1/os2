2b9b26edfe50d39c778e368c4999861d4eb0461f
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-08 02:15:30 +0300

    update

--- src/app/lang.c
+++ src/app/lang.c
@@ -1,21 +1,4 @@
 
-// would be super cool if we could make it so that each instruction is an
-// "offset" in an array of function pointers, so that we can execute the
-// function right away without any ifs or buts
-
-// as for the "instructions", we could instead of having an instruction
-// take a look into the next instruction and take it as an argument,
-// we could use some addresses the way CPUs use rax, rbx, ...
-// say, 0x00, 0x01, 0x02, ...
-
-// typedef enum{
-//     lang$INST_SET_PTR,
-//     lang$INST_SET_U32,
-//     lang$INST_PUT_CHAR,
-//     lang$INST_ADD_U32,
-//     // lang$INST_GET_CHAR,
-// }lang$instruction_t;
-
 typedef struct{
     uint8_t mem[1024 * 10]; // it is assumed that there is enough space for the first couple of necessary "registers"
 
@@ -41,23 +24,6 @@ void lang$inst$putchar_0x01(lang$program_data_t * ctx){
     uint8_t ch = ctx->mem[0x01];
     out$ch((char) ch);
 }
-
-// inline uint32_t lang$get_u32(lang$program_data_t * ctx){
-//     return * (uint32_t *) & ctx->mem[ctx->mem_ptr];
-// }
-
-// inline void lang$set_u32(lang$program_data_t * ctx, uint32_t value){
-//     * (uint32_t *) & ctx->mem[ctx->mem_ptr] = value;
-// }
-
-// inline void lang$put_char(lang$program_data_t * ctx){
-//     unsigned char ch = ctx->mem[ctx->mem_ptr];
-//     out$ch((char) ch);
-// }
-
-// inline void lang$add_u32(lang$program_data_t * ctx, uint32_t num){
-//     lang$set_u32(ctx, lang$get_u32(ctx) + num);
-// }
 
 typedef void (* lang$instruction_function_t) (lang$program_data_t *);
 
@@ -115,21 +81,6 @@ bool lang$program_data_t$exec(lang$program_data_t * ctx, size_t number_of_instru
 
 err_t lang$main(void){
 
-    // uint32_t code[] = {
-    //     lang$INST_SET_PTR,
-    //     0x40u,
-
-    //     lang$INST_SET_U32,
-    //     69,
-
-    //     lang$INST_PUT_CHAR,
-
-    //     lang$INST_ADD_U32,
-    //     1u,
-
-    //     lang$INST_PUT_CHAR,
-    // };
-
     lang$instruction_t code[] = {
         lang$INST_ADD_48_TO_0x01,
         lang$INST_PUTCHAR_0x01,
@@ -143,37 +94,6 @@ err_t lang$main(void){
     lang$program_data_t$init(ctx, code, LENOF(code));
 
     while(!lang$program_data_t$exec(ctx, 1)){}
-
-    // for(size_t idx=0; idx<LENOF(code); ++idx){
-    //     lang$instruction_t instruction = code[idx];
-
-    //     switch(instruction){
-
-    //         case lang$INST_SET_PTR:{
-    //             uint32_t addr = code[++idx]; // possible out of bound
-    //             err_t err = lang$set_mem_ptr(ctx, addr);
-    //             if(err){
-    //                 out$cstr("attempt to set the pointer to invalid location\n");
-    //                 return err;
-    //             }
-    //         }break;
-
-    //         case lang$INST_SET_U32:{
-    //             uint32_t value = code[++idx]; // possible out of bound
-    //             lang$set_u32(ctx, value);
-    //         }break;
-
-    //         case lang$INST_PUT_CHAR:{
-    //             lang$put_char(ctx);
-    //         }break;
-
-    //         case lang$INST_ADD_U32:{
-    //             uint32_t num = code[++idx]; // possible out of bound
-    //             lang$add_u32(ctx, num);
-    //         }break;
-
-    //     }
-    // }
 
     return err$OK;
 
