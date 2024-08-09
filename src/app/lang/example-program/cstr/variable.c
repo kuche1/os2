1ae25be9e4527ab662fd845324f95e734c652f02
kuche1 <znamsiparolata4esno@abv.bg>
2024-08-09 16:45:18 +0300

    update

--- src/app/lang/example-program/cstr/variable.c
+++ src/app/lang/example-program/cstr/variable.c
@@ -1,54 +1,45 @@
 
 // prints "Hello World\n"
 
-
-// // this is the original hello world
-// "out$arg 72\n"
-// "out$arg 101\n"
-// "out$arg 108\n"
-// "out$arg 108\n"
-// "out$arg 111\n"
-// "out$arg 32\n"
-// "out$arg 87\n"
-// "out$arg 111\n"
-// "out$arg 114\n"
-// "out$arg 108\n"
-// "out$arg 100\n"
-// "out$arg 10\n"
-
-
-
-// "var H\n"
-// "H 72\n"
-
-// "var e\n"
-// "e 101\n"
-
-// "var l\n"
-// "l 108\n"
-
-// "var o\n"
-// "o 111\n"
-
-// "var new-line\n"
-// "new-line 10\n"
-
-// // "out$arg H\n"
-// // "out$arg e\n"
-// // "out$arg l\n"
-// // "out$arg l\n"
-// // "out$arg o\n"
-// // "out$arg new-line\n"
-
-// "out$cell 16\n"
-// "out$cell 17\n"
-// "out$cell 18\n"
-// "out$cell 18\n"
-// "out$cell 19\n"
-// "out$cell 20\n"
-
-
 "var H\n"
 "H 72\n"
 
+"var e\n"
+"e 101\n"
+
+"var l\n"
+"l 108\n"
+
+"var o\n"
+"o 111\n"
+
+"var w\n"
+"w 119\n"
+
+"var space\n"
+"space 32\n"
+
+"var W\n"
+"W 87\n"
+
+"var r\n"
+"r 114\n"
+
+"var d\n"
+"d 100\n"
+
+"var new-line\n"
+"new-line 10\n"
+
 "out$cell H\n"
+"out$cell e\n"
+"out$cell l\n"
+"out$cell l\n"
+"out$cell o\n"
+"out$cell space\n"
+"out$cell W\n"
+"out$cell o\n"
+"out$cell r\n"
+"out$cell l\n"
+"out$cell d\n"
+"out$cell new-line\n"
